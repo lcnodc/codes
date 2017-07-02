ec4d28a804bf4b17e17e2337d7e94ed1bc2d32fc
Luciano <lcno.dc@gmail.com>
2017-07-02 09:38:31 -0300

    Update odd_or_even.py

--- 09-revisao/practice_python/odd_or_even.py
+++ 09-revisao/practice_python/odd_or_even.py
@@ -7,12 +7,15 @@ message to the user. Hint: how does an even / odd number react
 differently when divided by 2?
 
 Extras:
-
     - If the number is a multiple of 4, print out a different message.
     - Ask the user for two numbers: one number to check (call it num)
     and one number to divide by (check). If check divides evenly into
     num, tell that to the user. If not, print a different appropriate
     message.
+    
+Credits:
+    Michele Pratusevich
+    http://www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html
 """
 
 print("\nVerificando se é par/impar: ")
