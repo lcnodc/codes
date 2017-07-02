63e8a2de3a9b8c0301ba070c84fd360994ca09a2
Luciano <lcno.dc@gmail.com>
2017-07-02 09:39:24 -0300

    Update character_input.py

--- 09-revisao/practice_python/character_input.py
+++ 09-revisao/practice_python/character_input.py
@@ -7,13 +7,16 @@ Print out a message addressed to them that tells them the year that they
 will turn 100 years old.
 
 Extras:
-
     Add on to the previous program by asking the user for another number
     and printing out that many copies of the previous message. (Hint:
     order of operations exists in Python)
     Print out that many copies of the previous message on separate
     lines.
     (Hint: the string "\n is the same as pressing the ENTER button)
+    
+Credits:
+    Michele Pratusevich
+    http://www.practicepython.org/exercise/2014/01/29/01-character-input.html
 
 """
 
